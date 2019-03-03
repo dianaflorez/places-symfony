ef72b100480ece14f126b48df4ee81ccedffca60
Diana <dianaflorezbravo@gmail.com>
2019-03-03 18:45:05 -0500

    Initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homeAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/index.html.twig');
+        // return $this->render('default/index.html.twig', [
+        //     'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        // ]);
+    }
+
+    /**
+     * @Route("/nosotros", name="nosotros")
+     */
+    public function nosotrosAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/nosotros.html.twig');
+    }
+}
